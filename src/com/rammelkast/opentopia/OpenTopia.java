7854ff0a305009db2a187d211610db00816773a6
Marc Sollie <ik@marcsollie.nl>
2017-09-25 10:01:44 +0200

    Added Javadoc
    Added javadoc for onEnable and onDisable in OpenTopia.java

--- src/com/rammelkast/opentopia/OpenTopia.java
+++ src/com/rammelkast/opentopia/OpenTopia.java
@@ -4,11 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class OpenTopia extends JavaPlugin {
 
+	/**
+	 * Called when the plugin is enabled.
+	 */
 	@Override
 	public void onEnable() { 
 
 	}
 	
+	/**
+	 * Called when the plugin is disabled.
+	 */
 	@Override
 	public void onDisable() {
 		
