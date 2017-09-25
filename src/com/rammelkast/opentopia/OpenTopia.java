1e6d039b21d8e51f564d7a487a640e55279754fb
Marc Sollie <ik@marcsollie.nl>
2017-09-25 09:59:49 +0200

    Fixed tabs
    Seriously, I can understand 4 spaces or 2 spaces as indents. But who
    uses 3? Lol

--- src/com/rammelkast/opentopia/OpenTopia.java
+++ src/com/rammelkast/opentopia/OpenTopia.java
@@ -3,9 +3,9 @@ package com.rammelkast.opentopia;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OpenTopia extends JavaPlugin {
-  
-  @Override
-  public void onEnable() { 
-  
-  }
+
+	@Override
+	public void onEnable() { 
+
+	}
 }
